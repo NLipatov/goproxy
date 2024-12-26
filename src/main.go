100d150f960bf723bf9fdcd5938f3413ab122e7c
Nikita Lipatov <6stringsohei@gmail.com>
2024-12-26 23:38:11 +0300

    using one mode on startup

--- src/main.go
+++ src/main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"goproxy/Application"
 	data_access "goproxy/DataAccess"
+	"goproxy/DataAccess/Repositories"
 	"goproxy/Infrastructure"
 	"log"
 	"os"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 1 && args[0] == "migrate" {
-		db, err := data_access.ConnectDB()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer func(db *sql.DB) {
-			_ = db.Close()
-		}(db)
-
-		data_access.Migrate(db)
-		return
+	mode := os.Getenv("MODE")
+	switch mode {
+	case "migrate":
+		applyMigrations()
+	case "proxy":
+		startHttpProxy()
+	case "rest-api":
+		startHttpRestApi()
+	default:
+		log.Fatalf("Unsupported mode: %s", mode)
 	}
+}
 
+func applyMigrations() {
+	db, err := data_access.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+
+	data_access.Migrate(db)
+	return
+}
+
+func startHttpProxy() {
 	port := os.Getenv("HTTP_LISTENER_PORT")
 	if port == "" {
 		log.Fatalf("'HTTP_LISTENER_PORT' env var must be set")
@@ -30,6 +45,29 @@ func main() {
 
 	listener := Infrastructure.NewHttpListener()
 	err := listener.ServePort(port)
+	if err != nil {
+		log.Printf("Failed serving port: %v", err)
+	}
+}
+
+func startHttpRestApi() {
+	port := os.Getenv("HTTP_REST_API_PORT")
+	if port == "" {
+		log.Fatalf("'HTTP_REST_API_PORT' env var must be set")
+	}
+
+	db, err := data_access.ConnectDB()
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userRepository := Repositories.NewUserRepository(db)
+	useCases := Application.NewUserUseCases(userRepository)
+	restApiListener := Infrastructure.NewHttpRestApiListener(useCases)
+	err = restApiListener.ServePort(port)
 	if err != nil {
 		log.Printf("Failed serving port: %v", err)
 	}
