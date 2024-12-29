0af66a16a06d0f24f9497241bb0b2ba58ac3e83f
Nikita Lipatov <6stringsohei@gmail.com>
2024-12-29 17:29:04 +0300

    changed v3_outbox_table.sql

--- src/dal/migrations/v3_outbox_table.sql
+++ src/dal/migrations/v3_outbox_table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE outbox (
     id SERIAL PRIMARY KEY,
-    event_type VARCHAR(255) NOT NULL,
     payload JSONB NOT NULL,
     published BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT now()
