9416381b34411290506a4447fff9a5e76d82ba51
Nikita <6stringsohei@gmail.com>
2025-01-05 20:06:39 +0300

    new method added

--- src/domain/aggregates/UserPlan.go
+++ src/domain/aggregates/UserPlan.go
@@ -23,12 +23,19 @@ func NewUserPlan(id, userId, planId int, validTo, createdAt time.Time) (UserPlan
 func (up *UserPlan) Id() int {
 	return up.id
 }
+
 func (up *UserPlan) UserId() int {
 	return up.userId
 }
+
 func (up *UserPlan) PlanId() int {
 	return up.planId
 }
+
 func (up *UserPlan) ValidTo() time.Time {
 	return up.validTo
+}
+
+func (up *UserPlan) ProlongDays(days int) {
+	up.validTo = up.validTo.AddDate(0, 0, days)
 }
