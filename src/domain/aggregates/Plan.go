1e321d1ede3116ac30ec8a94fe398e1d0d3efd03
Nikita <6stringsohei@gmail.com>
2025-01-05 20:25:42 +0300

    enforced aggregate invariants

--- src/domain/aggregates/Plan.go
+++ src/domain/aggregates/Plan.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Plan struct {
-	id           int
-	name         string
-	limitBytes   valueobjects.PlanBytesLimit
-	durationDays int
-	createdAt    time.Time
+	id         int
+	name       valueobjects.PlanName
+	limitBytes valueobjects.PlanBytesLimit
+	duration   valueobjects.PlanDuration
+	createdAt  time.Time
 }
 
 func NewPlan(id int, name string, limitBytes int64, durationDays int) (Plan, error) {
@@ -19,12 +19,22 @@ func NewPlan(id int, name string, limitBytes int64, durationDays int) (Plan, err
 		return Plan{}, limitErr
 	}
 
+	planName, nameErr := valueobjects.ParsePlanNameFromString(name)
+	if nameErr != nil {
+		return Plan{}, nameErr
+	}
+
+	planDuration, durationErr := valueobjects.ParsePlanDurationFromDays(durationDays)
+	if durationErr != nil {
+		return Plan{}, durationErr
+	}
+
 	return Plan{
-		id:           id,
-		name:         name,
-		limitBytes:   limit,
-		durationDays: durationDays,
-		createdAt:    time.Now().UTC(),
+		id:         id,
+		name:       planName,
+		limitBytes: limit,
+		duration:   planDuration,
+		createdAt:  time.Now().UTC(),
 	}, nil
 }
 
@@ -33,7 +43,7 @@ func (p *Plan) Id() int {
 }
 
 func (p *Plan) Name() string {
-	return p.name
+	return p.name.Name()
 }
 
 func (p *Plan) LimitBytes() int64 {
@@ -41,7 +51,7 @@ func (p *Plan) LimitBytes() int64 {
 }
 
 func (p *Plan) DurationDays() int {
-	return p.durationDays
+	return p.duration.DurationDays()
 }
 
 func (p *Plan) CreatedAt() time.Time {
