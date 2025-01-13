947ef5966cfcb2c82a7262b22d3df68c5de503bb
Nikita <6stringsohei@gmail.com>
2025-01-13 19:30:22 +0300

    auth service tests

--- src/infrastructure/services/auth_service.go
+++ src/infrastructure/services/auth_service.go
@@ -5,8 +5,12 @@ import (
 	"goproxy/application"
 	"goproxy/domain/aggregates"
 	"goproxy/domain/valueobjects"
+	"os"
+	"strconv"
 	"time"
 )
+
+const defaultValidateCacheTTL = time.Second * 10
 
 type validateResult struct {
 	result bool
@@ -14,20 +18,32 @@ type validateResult struct {
 }
 
 type AuthService struct {
-	cryptoService application.CryptoService
-	validateCache application.CacheWithTTL[validateResult]
+	cryptoService    application.CryptoService
+	validateCache    application.CacheWithTTL[validateResult]
+	validateCacheTTL time.Duration
 }
 
 func NewAuthService(cryptoService application.CryptoService) *AuthService {
+	validateCacheTTL := defaultValidateCacheTTL
+	validateTtlEnv := os.Getenv("AUTH_SERVICE_VALIDATE_TTL_MS")
+	if validateTtlEnv != "" {
+		ttlMillis, err := strconv.Atoi(validateTtlEnv)
+		if err == nil {
+			validateCacheTTL = time.Duration(ttlMillis) * time.Millisecond
+		}
+	}
+
 	return &AuthService{
-		cryptoService: cryptoService,
-		validateCache: NewMapCacheWithTTL[validateResult](),
+		cryptoService:    cryptoService,
+		validateCache:    NewMapCacheWithTTL[validateResult](),
+		validateCacheTTL: validateCacheTTL,
 	}
 }
 
 func (authService *AuthService) AuthorizeBasic(user aggregates.User, credentials valueobjects.BasicCredentials) (bool, error) {
 	cacheKey := fmt.Sprintf("%s:%x", credentials.Username, user.PasswordSalt())
-	if cached, cachedErr := authService.validateCache.Get(cacheKey); cachedErr == nil {
+	cached, cachedErr := authService.validateCache.Get(cacheKey)
+	if cachedErr == nil {
 		return cached.result, cached.err
 	}
 
@@ -37,7 +53,7 @@ func (authService *AuthService) AuthorizeBasic(user aggregates.User, credentials
 	}
 
 	_ = authService.validateCache.Set(cacheKey, validateResult{true, nil})
-	_ = authService.validateCache.Expire(cacheKey, time.Second*10)
+	_ = authService.validateCache.Expire(cacheKey, authService.validateCacheTTL)
 
 	return true, nil
 }
