d4358aa63554e953237940a560e344c04c31c987
Nikita <6stringsohei@gmail.com>
2025-01-13 18:27:45 +0300

    AuthService - cache added for caching validation method call results

--- src/infrastructure/services/auth_service.go
+++ src/infrastructure/services/auth_service.go
@@ -5,23 +5,39 @@ import (
 	"goproxy/application"
 	"goproxy/domain/aggregates"
 	"goproxy/domain/valueobjects"
+	"time"
 )
+
+type validateResult struct {
+	result bool
+	err    error
+}
 
 type AuthService struct {
 	cryptoService application.CryptoService
+	validateCache application.CacheWithTTL[validateResult]
 }
 
 func NewAuthService(cryptoService application.CryptoService) *AuthService {
 	return &AuthService{
 		cryptoService: cryptoService,
+		validateCache: NewMapCacheWithTTL[validateResult](),
 	}
 }
 
 func (authService *AuthService) AuthorizeBasic(user aggregates.User, credentials valueobjects.BasicCredentials) (bool, error) {
+	cacheKey := fmt.Sprintf("%s:%x", credentials.Username, user.PasswordSalt())
+	if cached, cachedErr := authService.validateCache.Get(cacheKey); cachedErr == nil {
+		return cached.result, cached.err
+	}
+
 	isPasswordValid := authService.cryptoService.ValidateHash(user.PasswordHash(), user.PasswordSalt(), credentials.Password)
 	if !isPasswordValid {
 		return false, fmt.Errorf("invalid credentials")
 	}
+
+	_ = authService.validateCache.Set(cacheKey, validateResult{true, nil})
+	_ = authService.validateCache.Expire(cacheKey, time.Second*10)
 
 	return true, nil
 }
