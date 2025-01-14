706bd12926098b08eb3da155b2c8c9756d9ca07d
Nikita <6stringsohei@gmail.com>
2025-01-14 14:23:19 +0300

    users schema update: added email column, added indexes

--- src/dal/migrations/proxydb/v2_users_table.sql
+++ src/dal/migrations/proxydb/v2_users_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE public.users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password_hash BYTEA NOT NULL,
     password_salt BYTEA NOT NULL,
     created_at TIMESTAMP DEFAULT now()
 );
+
+CREATE INDEX idx_email on users(email);
+CREATE INDEX idx_username on users(username);
