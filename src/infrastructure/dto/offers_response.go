d55ec4a440f2b62ae3e02ffc9394b7284970e512
Nikita <6stringsohei@gmail.com>
2025-01-21 20:45:33 +0300

    dto map for Offer and Price

--- src/infrastructure/dto/offers_response.go
+++ src/infrastructure/dto/offers_response.go
@@ -1,5 +1,7 @@
 package dto
 
+import "goproxy/domain/lavatopsubdomain/lavatopvalueobjects"
+
 type PriceDto struct {
 	Currency    string  `json:"currency"`
 	Amount      float64 `json:"amount"`
@@ -7,7 +9,7 @@ type PriceDto struct {
 }
 
 type OfferResponse struct {
-	ID     string     `json:"id"`
+	Id     string     `json:"id"`
 	Name   string     `json:"name"`
 	Prices []PriceDto `json:"prices"`
 }
@@ -18,4 +20,25 @@ type ProductResponse struct {
 }
 type GetOffersResponse struct {
 	Items []ProductResponse `json:"items"`
+}
+
+func ToOfferResponse(offer lavatopvalueobjects.Offer) OfferResponse {
+	prices := make([]PriceDto, len(offer.Prices()))
+	for i, price := range offer.Prices() {
+		prices[i] = ToPriceDto(price)
+	}
+
+	return OfferResponse{
+		Id:     offer.ExtId(),
+		Name:   offer.Name(),
+		Prices: prices,
+	}
+}
+
+func ToPriceDto(price lavatopvalueobjects.Price) PriceDto {
+	return PriceDto{
+		Currency:    price.Currency().String(),
+		Amount:      float64(price.Cents()) / 100.0,
+		Periodicity: price.Periodicity().String(),
+	}
 }
