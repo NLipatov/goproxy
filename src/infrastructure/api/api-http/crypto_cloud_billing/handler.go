f58d0fb614e80513c80ffabd47c9db2a903ab63f
Nikita <6stringsohei@gmail.com>
2025-01-26 20:47:31 +0300

    implemented post back handler

--- src/infrastructure/api/api-http/crypto_cloud_billing/handler.go
+++ src/infrastructure/api/api-http/crypto_cloud_billing/handler.go
@@ -5,6 +5,8 @@ import (
 	"goproxy/application/payments/crypto_cloud"
 	"goproxy/application/payments/crypto_cloud/crypto_cloud_commands"
 	"goproxy/infrastructure/api/api-http/crypto_cloud_billing/crypto_cloud_billing_dto"
+	"goproxy/infrastructure/payments/crypto_cloud/crypto_cloud_dto"
+	"log"
 	"net/http"
 )
 
@@ -20,6 +22,7 @@ func NewHandler(paymentService crypto_cloud.PaymentProvider) Handler {
 
 func (h Handler) IssueInvoice(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		log.Printf("PostBack handling: 405 as method not allowed")
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
@@ -27,7 +30,9 @@ func (h Handler) IssueInvoice(w http.ResponseWriter, r *http.Request) {
 	var dto crypto_cloud_billing_dto.IssueInvoiceCommandDto
 	decoder := json.NewDecoder(http.MaxBytesReader(w, r.Body, 512))
 	if err := decoder.Decode(&dto); err != nil {
+		log.Printf("PostBack handling: 400 as body object scheme is invalid: %s", err.Error())
 		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("invalid request body"))
 		return
 	}
 
@@ -39,16 +44,51 @@ func (h Handler) IssueInvoice(w http.ResponseWriter, r *http.Request) {
 
 	result, resultErr := h.paymentService.IssueInvoice(cmd)
 	if resultErr != nil {
+		log.Printf("PostBack handling: 400 as body object is invalid: %s", resultErr.Error())
 		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte(resultErr.Error()))
+		_, _ = w.Write([]byte("invalid request body"))
 		return
 	}
 
+	log.Printf("PostBack handling: 200 OK")
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(result)
+	return
 }
 
 func (h Handler) HandlePostBack(w http.ResponseWriter, r *http.Request) {
-	panic("implement me")
+	if r.Method != "POST" {
+		log.Printf("PostBack handling: 405 as method not allowed")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var dto crypto_cloud_dto.PostbackRequest
+	decoder := json.NewDecoder(http.MaxBytesReader(w, r.Body, 8_000))
+	if err := decoder.Decode(&dto); err != nil {
+		log.Printf("PostBack handling: 400 as body object scheme is invalid: %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("invalid request body"))
+		return
+	}
+
+	cmd := crypto_cloud_commands.PostBackCommand{
+		OrderID: dto.OrderID,
+		Token:   dto.Token,
+	}
+
+	handlePostBackErr := h.paymentService.HandlePostBack(cmd)
+	if handlePostBackErr != nil {
+		log.Printf("PostBack handling: 400 as body object data is invalid: %s", handlePostBackErr.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("invalid request body"))
+		return
+	}
+
+	log.Printf("PostBack handling: 200 OK")
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte("{'message': 'Postback received'}"))
+	return
 }
