2a6f5273ab44c485e3c582b87573423a374cf355
Nikita <6stringsohei@gmail.com>
2025-01-26 17:37:17 +0300

    checking all the field in create_invoice_response_test.go

--- src/infrastructure/payments/crypto_cloud/crypto_cloud_dto/create_invoice_response.go
+++ src/infrastructure/payments/crypto_cloud/crypto_cloud_dto/create_invoice_response.go
@@ -3,24 +3,25 @@ package crypto_cloud_dto
 type InvoiceResponse struct {
 	Status string `json:"status"`
 	Result struct {
-		UUID             string  `json:"uuid"`
-		Created          string  `json:"created"`
-		Address          string  `json:"address,omitempty"`
-		ExpiryDate       string  `json:"expiry_date"`
-		SideCommission   string  `json:"side_commission"`
-		SideCommissionCC string  `json:"side_commission_cc"`
-		Amount           float64 `json:"amount"`
-		AmountUSD        float64 `json:"amount_usd"`
-		AmountInFiat     float64 `json:"amount_in_fiat"`
-		Fee              float64 `json:"fee"`
-		FeeUSD           float64 `json:"fee_usd"`
-		ServiceFee       float64 `json:"service_fee"`
-		ServiceFeeUSD    float64 `json:"service_fee_usd"`
-		FiatCurrency     string  `json:"fiat_currency"`
-		Status           string  `json:"status"`
-		IsEmailRequired  bool    `json:"is_email_required"`
-		Link             string  `json:"link"`
-		Currency         struct {
+		UUID                  string  `json:"uuid"`
+		Created               string  `json:"created"`
+		Address               string  `json:"address,omitempty"`
+		ExpiryDate            string  `json:"expiry_date"`
+		SideCommission        string  `json:"side_commission"`
+		SideCommissionService string  `json:"side_commission_service"`
+		TypePayments          string  `json:"type_payments"`
+		Amount                float64 `json:"amount"`
+		AmountUSD             float64 `json:"amount_usd"`
+		AmountInFiat          float64 `json:"amount_in_fiat"`
+		Fee                   float64 `json:"fee"`
+		FeeUSD                float64 `json:"fee_usd"`
+		ServiceFee            float64 `json:"service_fee"`
+		ServiceFeeUSD         float64 `json:"service_fee_usd"`
+		FiatCurrency          string  `json:"fiat_currency"`
+		Status                string  `json:"status"`
+		IsEmailRequired       bool    `json:"is_email_required"`
+		Link                  string  `json:"link"`
+		Currency              struct {
 			ID       int    `json:"id"`
 			Code     string `json:"code"`
 			FullCode string `json:"fullcode"`
